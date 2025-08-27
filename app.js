8fc8ba2a02e24593cd7a2b36650da85d83d3a86f
JoseZL <josezamora485@gmail.com>
2025-08-27 11:59:26 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+let oneEuroIs = {
+    "JPY": 156.5, // Japan yen
+    "USD": 1.07, // US dollar
+    "GBP": 0.87, // British pound
+};
+
+function fromDollarToYen(dollars) {
+    return (dollars / oneEuroIs["USD"]) * oneEuroIs["JPY"];
+}
+
+function fromEuroToDollar(euros) {
+    return euros * oneEuroIs["USD"];
+}
+
+function fromYenToPound(yen) {
+    return (yen / oneEuroIs["JPY"]) * oneEuroIs["GBP"];
+}
+
+// Tenemos que incluir la función en el exports para que sea exportada a otros archivos como test.js
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound }
